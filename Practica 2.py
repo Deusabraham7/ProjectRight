30949dcbb10a5f57eec17456960d613ed7882987
Deusabraham7 <abagarciarosas7@gmail.com>
2023-03-01 10:53:53 -0600

    ejercicio 2

---
+++ Practica 2.py
@@ -0,0 +1,10 @@
+
+numero  = input("Ingresa un numero:  ")
+numero = int(numero)
+
+if numero >100 and numero <1000000:
+        print("Correcto")
+
+
+else:
+ print("Error")
--- ProjectJavaAAGR/src/main/java/org/example/Main.java
+++ ProjectJavaAAGR/src/main/java/org/example/Main.java
@@ -5,75 +5,75 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-       /* System.out.println("Hello world!");
-        System.out.println("Abraham Alonso Garcia Rosas ");
-        int miEntero = 10;
-        float miPuntoFlotante = 0.5f;
-        double miDecimal = 22.5;
-        String miTexto = "Hola soy un texto en java";
-        char miCaracter = 'a';
-        Scanner miEscanner = new Scanner(System.in);
 
-        int numero;
-        numero = miEscanner.nextInt();
+        //Desarrollar un programa que le pida al usuario su nombre , su edad, domicilio,precio de vivienda
+        //calcular el predial 1.8% de impuesto
+        //mostrar la informacion del usuario
 
-        System.out.println(" Mi numero que acabo de ingresar es : " + numero);*/
-
-        //desarrollar un programa en java que lea 2 numeros , los sume y me muestre el resultado
-        /*Scanner Escanner = new Scanner(System.in);
+        //desarrolla un programa que le pregunte al usuario 3 numeros
+        // y decirle cual es mayor de los 3
+       /*
+        Scanner escanner = new Scanner(System.in);
 
 
-        int numero1;
-        int numero2;
+        double numero1;
+        double numero2;
+        double numero3;
+        double numero4;
 
 
-        numero1 = Escanner.nextInt();
-        numero2 = Escanner.nextInt();
+        System.out.println("Escribe el primer numero");
+        numero1=escanner.nextDouble();
+        System.out.println("Escribe el segundo numero");
+        numero2=escanner.nextDouble();
+        System.out.println("Escribe el tercer numero");
+        numero3=escanner.nextDouble();
+        System.out.println("Escribe el cuarto numero");
+        numero4=escanner.nextDouble();
 
 
-        int suma = numero1*numero2;
+        if (numero1>numero2 && numero1>numero3 && numero1>numero4){
+            System.out.println("El numero mayor es 1");
+        }
+        else{
+            if(numero2>numero1&&numero2>numero3 && numero2>numero4){
+                System.out.println("El numero 2 es mayor");
+            }
+            else {
+
+                if (numero3>numero1 && numero3>numero2 && numero3>numero4){
+                    System.out.println("el numero 3 es el mayor");
+                }
+                else{
+                    if (numero4>numero1 && numero4>numero2 && numero4>numero3){
+                        System.out.println("el numero 4 es el mayor");
+                    }
+                    else{
+                        System.out.println("ningun numero es mayor");
+                    }
+                }
 
 
-
-
-        System.out.println(" El resultado de la suma es: "+suma);*/
-        //desarrollar un programa que le pida al usuario , su nombre ,  su edad, su peso y su altura , que calcule el imc y que me de una carta de tod lo visto
-
-        Scanner Escanner = new Scanner(System.in);
-
-        String Nombre;
-        int Edad;
-        double Peso;
-        double Altura;
-
-        System.out.println("Ingrese su nombre");
-        Nombre = Escanner.nextLine();
-        System.out.println("Ingrese su edad");
-        Edad = Escanner.nextInt();
-        System.out.println("Ingrese su Altura");
-        Altura = Escanner.nextDouble();
-        System.out.println("Igrese su peso");
-        Peso = Escanner.nextDouble();
-
-
-        double imc = Peso/Math.pow(Altura,2);
-        System.out.println("El resultado de el Imc es:"+imc);
-
-
-
-
-        System.out.println(" El nombre es:"+Nombre);
-        System.out.println("La edad es:"+Edad);
-        System.out.println(" La altura es:"+Altura);
-        System.out.println("El peso es :"+Peso);
+            }
+        }
+        if (numero1==numero2 && numero1==numero3 ){
+            System.out.println("Todos los numeros son iguales");
+        }
 
 
 
 
 
 
+        System.out.println("El numero 1 es :"+numero1);
+        System.out.println("El numero 2 es :"+numero2);
+        System.out.println("El numero 3 es :"+numero3);
+        System.out.println("El numero 4 es :"+numero4);
 
-
+        */
+        for (int i=0;i<100;i++){
+            System.out.println("i vale: "+i);
+        }
 
 
 
