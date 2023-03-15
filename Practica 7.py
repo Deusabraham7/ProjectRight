17a71b48e9b053e71a8847b3371ea4cb34a9fe25
Deusabraham7 <abagarciarosas7@gmail.com>
2023-03-15 12:01:09 -0600

    Ejercicios 5 y 6

---
+++ Practica 7.py
@@ -0,0 +1,38 @@
+l = input("Ingrese un numero de lados ")
+l= int(l)
+
+if l==3:
+    print("Triangulo")
+else:
+        
+    if l==4 :
+      print("Cuadrado")
+    else:
+        if l==5 :
+            print("Pentagono")
+        else:
+            
+            if l==6 :
+                print("Hexagono")
+            else:
+                
+                if l==7 :
+                    print("Heptagono")
+                else:
+                    
+                    if l==8 :
+                        print("Octagono")
+                    else:
+                        if l==10 :
+                            print("decagono")
+                        else:
+                            
+
+                            if l==12 :
+                                print("Dodecagono")
+                            else:
+                                print("Adios")
+                                
+
+
+        
